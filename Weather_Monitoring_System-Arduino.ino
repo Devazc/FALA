3a83b545d6f4a7307482855975b7bf5f6a9b16a1
Deva Chandragiri <122866331+Devazc@users.noreply.github.com>
2023-01-17 17:51:00 +0530

    Add files via upload

---
+++ Weather_Monitoring_System-Arduino.ino
@@ -0,0 +1,103 @@
+/*Weather monitoring system 
+ *Medium - Thingspeak.
+
+*/
+
+#include <SFE_BMP180.h>
+#include <Wire.h>
+#include <ESP8266WiFi.h>
+#include "DHT.h"
+
+
+
+DHT dht(D3, DHT11);
+SFE_BMP180 bmp;
+double T, P;
+char status;
+WiFiClient client;
+
+String apiKey = "RRL0K70OVHOBSH8D";
+const char *ssid =  "WIFI USERNAME";
+const char *pass =  "WIFI PASSWORD";
+const char* server = "api.thingspeak.com";
+
+
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+  bmp.begin();
+  Wire.begin();
+  dht.begin();
+  WiFi.begin(ssid, pass);
+
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+}
+
+void loop() {
+  //BMP180 sensor
+  status =  bmp.startTemperature();
+  if (status != 0) {
+    delay(status);
+    status = bmp.getTemperature(T);
+
+    status = bmp.startPressure(3);// 0 to 3
+    if (status != 0) {
+      delay(status);
+      status = bmp.getPressure(P, T);
+      if (status != 0) {
+
+      }
+    }
+  }
+
+  //DHT11 sensor
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+ 
+
+
+  if (client.connect(server, 80)) {
+    String postStr = apiKey;
+    postStr += "&field1=";
+    postStr += String(t);
+    postStr += "&field2=";
+    postStr += String(h);
+    postStr += "&field3=";
+    postStr += String(P, 2);
+    postStr += "\r\n\r\n\r\n\r\n";
+
+    client.print("POST /update HTTP/1.1\n");
+    client.print("Host: api.thingspeak.com\n");
+    client.print("Connection: close\n");
+    client.print("X-THINGSPEAKAPIKEY: " + apiKey + "\n");
+    client.print("Content-Type: application/x-www-form-urlencoded\n");
+    client.print("Content-Length: ");
+    client.print(postStr.length());
+    client.print("\n\n\n\n");
+    client.print(postStr);
+
+    Serial.print("Temperature: ");
+    Serial.println(t);
+    Serial.print("Humidity: ");
+    Serial.println(h);
+    Serial.print("absolute pressure: ");
+    Serial.print(P, 2);
+    Serial.println("mb");
+    
+
+  }
+  client.stop();
+  delay(1000);
+}
